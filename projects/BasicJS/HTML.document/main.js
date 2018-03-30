3c6765fb7f5bfa0638758a21957ee2a0ed696f8a
SharifCoding <macbook@apples-mbp.home>
2018-03-30 18:38:10 +0100

    updated profile with mini JS exercises

---
+++ projects/BasicJS/HTML.document/main.js
@@ -0,0 +1,100 @@
+console.log(document.title);
+document.title = 'Basic HTML/DOM Design';
+console.log(document.title);
+
+/*
+body {
+  font-family: Arial, Helvetica, sans-serif; }
+*/
+var newBody = document.querySelector('body');
+newBody.style.fontFamily = 'Arial, Helvetica, sans-serif';
+newBody.style.backgroundColor = '#FFBB22';
+
+/*
+header {
+  font-size: 36px;
+  color: white;
+  background-color: #333;
+  padding: 0px 16px; }
+*/
+var newHeader = document.querySelector('header');
+newHeader.style.fontSize = '36px';
+newHeader.style.color = 'white';
+newHeader.style.backgroundColor = '#333';
+newHeader.style.padding = '0px 16px';
+
+/*
+ul {
+  list-style-type: none;
+  margin: 0px;
+  padding: 0px;
+  overflow: hidden;
+  background-color: #333; }
+*/
+var newUl = document.querySelector('nav ul');
+console.log(newUl);
+newUl.style.listStyleType = 'none';
+newUl.style.margin = '0px';
+newUl.style.padding = '0px';
+newUl.style.overflow = 'hidden';
+newUl.style.backgroundColor = '#333';
+
+/*
+li {
+  float: left; }
+  li a {
+    display: block;
+    color: white;
+    text-align: left;
+    padding: 14px 16px;
+    text-decoration: none; }
+    li a:hover {
+      background-color: #111; }
+*/
+var links = document.querySelectorAll('a');
+console.log(document);
+for(var i = 0; i < links.length; i++){
+links[i].style.float = 'left';
+links[i].style.float = 'left';
+links[i].style.display = 'block';
+links[i].style.color = 'white';
+links[i].style.textAlign = 'left';
+links[i].style.padding = '14px 16px';
+links[i].style.textDecoration = 'none';
+}
+function mouseOver(doc) {
+    doc.style.backgroundColor = '#111';
+}
+function mouseOut(doc) {
+    doc.style.backgroundColor = '#333';
+    newActive.style.backgroundColor = '#4CAF50';
+}
+
+/*
+.active {
+    background-color: #4CAF50; }
+*/
+var newActive = document.querySelector('.active');
+newActive.style.backgroundColor = '#4CAF50';
+
+/*
+figure > img {
+  max-width: 100%;
+  max-height: 100%; }
+*/
+var newFigureImg = document.querySelector('figure img');
+newFigureImg.style.maxWidth = '100%';
+newFigureImg.style.maxHeight = '100%';
+
+/*
+footer {
+  font-size: 36px;
+  color: white;
+  background-color: #333;
+  padding: 0px 16px; }
+*/
+var newFooter = document.querySelector('footer');
+newFooter.style.fontSize = '36px';
+newFooter.style.color = 'white';
+newFooter.style.backgroundColor = '#333';
+newFooter.style.padding = '0px 16px';
---
+++ projects/BasicJS/SocialAlert/main.js
@@ -0,0 +1,31 @@
+const charLimit = 140;
+const tweetBox = document.getElementById('tweet-box');
+const submit = document.getElementById('submit');
+const counter = document.getElementById('counter');
+
+counter.textContent = charLimit;
+
+submit.addEventListener('click', (() => {
+  window.alert('You tweeted: ' + tweetBox.value);
+}));
+
+tweetBox.addEventListener('input', (event) => {
+  const tweet = event.target.value;
+  
+  const charsRemaining = charLimit - tweet.length;
+  counter.textContent = charsRemaining;
+
+  if(charsRemaining < 0) {
+    console.log(charsRemaining);
+    submit.setAttribute('disabled', 'true');
+  } else {
+    submit.removeAttribute('disabled');
+  }
+})
+
+var newBody = document.querySelector('body');
+newBody.style.fontFamily = 'Arial, Helvetica, sans-serif';
+newBody.style.backgroundColor = '#FFBB22';
+newBody.style.textAlign = 'center';
+newBody.style.padding = '20px';
+newBody.style.height = '50px';
